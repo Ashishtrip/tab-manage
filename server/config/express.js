af9de4910220f1c9ee3ce62b391e62bd5fc92c93
Ashish <ashish.deo.tripathi@gmail.com>
2026-07-30 22:34:01 +0530

    chore: restrict CORS origins to development URLs and update server port to use environment variable configuration

--- server/config/express.js
+++ server/config/express.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-  origin: true,
+  origin: ['http://localhost:5173', 'http://127.0.0.1:5173'],
   credentials: true
 }));
 app.use('/api/auth', authRouter);
